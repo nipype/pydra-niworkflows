0fe7565c6e8d5447c6d3576634a7ebd429cb1a89
Tom Close <tom.g.close@gmail.com>
2024-04-22 16:03:45 +1000

    added in additional interface specs, altered some specs to handle special cases using find_replace

--- pydra/tasks/niworkflows/__init__.py
+++ pydra/tasks/niworkflows/__init__.py
@@ -1,39 +1,13 @@
-"""
-This is a basic doctest demonstrating that the package and pydra can both be successfully
-imported.
-
->>> import pydra.engine
->>> import pydra.tasks.niworkflows
-"""
-
-from warnings import warn
+import attrs
+from fileformats.generic import Directory, File
+import logging
 from pathlib import Path
-
-pkg_path = Path(__file__).parent.parent
-
-try:
-    from ._version import __version__
-except ImportError:
-    raise RuntimeError(
-        "pydra-niworkflows has not been properly installed, please run "
-        f"`pip install -e {str(pkg_path)}` to install a development version"
-    )
-if "post" not in __version__:
-    try:
-        from ._post_release import post_release
-    except ImportError:
-        try:
-            # For interface-only packages
-            from .auto._post_release import post_release
-        except ImportError:
-            pass
-        warn(
-            "Nipype interfaces haven't been automatically converted from their specs in "
-            f"`nipype-auto-conv`. Please run `{str(pkg_path / 'nipype-auto-conv' / 'generate')}` "
-            "to generated the converted Nipype interfaces in pydra.tasks.niworkflows.auto"
-        )
-    else:
-        __version__ += "post" + post_release
+from pydra.engine import Workflow
+from pydra.engine.specs import MultiInputObj
+import pydra.mark
 
 
-__all__ = ["__version__"]
+logger = logging.getLogger(__name__)
+
+
+
